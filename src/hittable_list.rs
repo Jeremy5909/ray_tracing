eccc92250d90ad27e52b867be41f577f55f381c9
Jeremy5909 <jeremyherczeg@gmail.com>
2024-05-02 10:48:18 -0700

    Revert "maybe make it more efficient"

--- src/hittable_list.rs
+++ src/hittable_list.rs
@@ -14,16 +14,17 @@ impl HittableList {
 
 impl Hittable for HittableList {
     fn hit(&self, r: &crate::ray::Ray, ray_t: Interval, rec: &mut HitRecord) -> bool {
+        let mut temp_rec = HitRecord::default();
+        let mut hit_anything = false;
         let mut closest_so_far = ray_t.max;
 
-        let hit_anything = self.objects.iter().any(|object| {
-            if object.hit(r, Interval::new(ray_t.min, closest_so_far), rec) {
-                closest_so_far = rec.t;
-                true
-            } else {
-                false
+        for object in &self.objects {
+            if object.hit(r, Interval::new(ray_t.min, closest_so_far), &mut temp_rec) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                *rec = temp_rec.clone();
             }
-        });
+        }
         hit_anything
     }
 }
